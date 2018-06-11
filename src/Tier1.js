ddd280e12e1396db4681581c6409f96af613d0cc
briecodes <hello@brie.work>
2018-06-10 22:15:31 -0400

    progress

--- src/Tier1.js
+++ src/Tier1.js
@@ -14,13 +14,23 @@ export default class Tier1 extends Component {
     }
   }
 
+  parentColor = (e) => {
+    if (e.target.className === "tier1"){
+      let color = getRandomColor();
+      this.setState({
+        color: color,
+        childColor: getReducedColor(color)
+      })
+    }
+  }
+
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={"#0F0"} />
-        <Tier2 color={"#0FF"} />
+      <div onClick={this.parentColor} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
+        <Tier2 color={this.state.childColor} />
+        <Tier2 color={this.state.childColor} />
       </div>
     )
   }
--- src/Tier2.js
+++ src/Tier2.js
@@ -8,7 +8,26 @@ export default class Tier2 extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      childColor: getReducedColor(this.props.color),
+      parentColor: this.props.color,
+      childColor: getReducedColor(this.props.color)
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (this.props.parentColor !== nextProps.color);
+  }
+
+  // componentWillUpdate(nextProps){
+  //   this.setState({
+  //     parentColor: nextProps.color,
+  //     childColor: getReducedColor(nextProps.color)
+  //   })
+  // }
+
+  doingSomething = (e) => {
+    console.log(e.target)
+    console.log("doing something");
+    if (e.target.className === "tier2"){
     }
   }
 
@@ -16,9 +35,9 @@ export default class Tier2 extends Component {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
-        <Tier3 color={"#F00"} />
+      <div onClick={this.doingSomething} className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
+        <Tier3 color={this.state.childColor} />
+        <Tier3 color={this.state.childColor} />
       </div>
     )
   }
